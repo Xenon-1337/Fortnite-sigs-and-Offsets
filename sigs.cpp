90ad0757cf1dfc6558efb84c19c3254d51908a9c
Xenon1337 <55765763+Xenon-1337@users.noreply.github.com>
2022-05-29 12:18:37 -0500

    Update sigs.cpp

--- sigs.cpp
+++ sigs.cpp
@@ -1,1 +1,7 @@
-
+Uworld - 48 89 05 ? ? ? ? 48 8B 4B 78
+FreeFN - 48 85 C9 0F 84 ? ? ? ? 53 48 83 EC 20 48 89 7C 24 30 48 8B D9 48 8B 3D ? ? ? ? 48 85 FF 0F 84 ? ? ? ? 48 8B 07 4C 8B 40 30 48 8D 05 ? ? ? ? 4C 3B C0
+ProjectWorldToScreen - E8 ? ? ? ? 4C 8D 5C 24 ? 41 88 06 49 8B 5B 20 49 8B 73 30 49 8B 7B 38
+LineOfSight - 48 89 5C 24 ? 48 89 74 24 ? 55 57 41 56 48 8B EC 48 83 EC 60 33 FF 4D 8B F0 48 89 7D 28
+GetNameByIndex - 48 89 5C 24 08 48 89 74 24 18 48 89 7C 24 20 41 56 48 81 EC 30
+BoneMatrix - E8 ? ? ? ? 0F 10 40 68
+Gobjects - 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1
